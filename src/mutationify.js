d01700ddba4e35d1f13ab7b5d104f25eae670d60
Jan Aagaard Meier <janzeh@gmail.com>
2017-10-16 14:09:46 +0200

    add: support for objects and arrays

--- src/mutationify.js
+++ src/mutationify.js
@@ -2,6 +2,10 @@ module.exports = function mutationify(object) {
   return '{'+Object.keys(object).reduce(function (result, key) {
     if (typeof object[key] === 'number') {
       result.push(`${key}: ${object[key]}`);
+    } else if (Array.isArray(object[key])) {
+      result.push(`${key}: [${object[key].map(mutationify)}]`)
+    } else if (typeof object[key] === 'object') {
+      result.push(`${key}: ${mutationify(object[key])}`)
     } else {
       result.push(`${key}: "${object[key]}"`);
     }
