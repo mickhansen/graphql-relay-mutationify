2685b4daeee0a9726c44eaa9a694d5bf4a19e2e6
Jan Aagaard Meier <janzeh@gmail.com>
2017-10-16 20:16:52 +0200

    add: support for objects and arrays (#1)

--- test/mutationify.test.js
+++ test/mutationify.test.js
@@ -5,7 +5,7 @@ var util = require('util');
 describe('mutationify', function () {
   var testInput = function (input, expectation) {
     it(util.inspect(input), function () {
-      assert(mutationify(input) === expectation);
+      assert(mutationify(input) === expectation, `Expected ${mutationify(input)} to produce ${expectation}`);
     });
   };
 
@@ -14,4 +14,20 @@ describe('mutationify', function () {
     lastName: 'Hansen',
     count: 30
   }, `{firstName: "Mick", lastName: "Hansen", count: 30}`)
+
+  testInput({
+    users: [{
+    firstName: 'Mick',
+    lastName: 'Hansen',
+    count: 30
+    }]
+  }, `{users: [{firstName: "Mick", lastName: "Hansen", count: 30}]}`)
+
+  testInput({
+    user: {
+    firstName: 'Mick',
+    lastName: 'Hansen',
+    count: 30
+    }
+  }, `{user: {firstName: "Mick", lastName: "Hansen", count: 30}}`)
 });
